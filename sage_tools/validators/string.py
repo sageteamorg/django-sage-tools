7b5b7488ffc2490aed474cda6df6e3d96b6bc2e4
Sepehr Akbarzadeh <sa.goldeneagle@gmail.com>
2024-08-01 20:39:30 +0330

    fix(validator): fix translation error for gettext_lazy

--- sage_tools/validators/string.py
+++ sage_tools/validators/string.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
 import re
 
 @deconstructible
